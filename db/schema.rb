ad161088276ed6fd606f1f3dde95a42adb25c5c8
Daichi <manoent11850@gmail.com>
2016-06-27 13:59:22 +0000

    add age

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602133159) do
+ActiveRecord::Schema.define(version: 20160627131048) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
